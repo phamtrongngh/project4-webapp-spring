5777c6f8d7e71471a3a877e0d7ddbac15773942b
letuyettrinh10c12 <letuyettrinh10c12@gmail.com>
2020-07-12 17:40:19 +0700

    Done Register on AuthController. Added postRegister on RestAut

--- src/main/java/controllers/AuthController.java
+++ src/main/java/controllers/AuthController.java
@@ -4,6 +4,7 @@ import Nghia.Util.CookieHelper;
 import Nghia.Util.RESTAuthorizeHelper;
 import Nghia.Util.RESTHelper;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -84,5 +85,21 @@ public class AuthController implements IController<Authorization> {
     public ModelAndView register() {
         return new ModelAndView("register");
     }
-
+    
+    @RequestMapping(value = "/Authorize/register", method = RequestMethod.POST)
+    public ModelAndView register(Authorization authorization, HttpServletRequest request) throws IOException{
+        
+        Map<String, String> address = new HashMap<>();
+        authorization.setPhone(request.getParameter("phone").toString());
+        authorization.setPassword(request.getParameter("password").toString());
+        authorization.setGender(Boolean.getBoolean(request.getParameter("gender").toString()));
+        authorization.setFullname(request.getParameter("fullname").toString());
+        address.put("city", request.getParameter("city"));
+        address.put("town", request.getParameter("town"));
+        address.put("ward", request.getParameter("ward"));
+        address.put("street", request.getParameter("street"));
+        authorization.setAddress(address);
+        rest.postRegister(authorization);
+        return new ModelAndView("login");
+    }
 }
