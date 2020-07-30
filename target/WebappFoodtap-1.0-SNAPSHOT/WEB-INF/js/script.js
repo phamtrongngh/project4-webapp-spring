111bd4946af6ec422419b96c7c5b5b7eeb41b7ee
unknown <phantaithinh@gmail.com>
2020-07-30 17:52:02 +0700

    like and draft

--- target/WebappFoodtap-1.0-SNAPSHOT/WEB-INF/js/script.js
+++ target/WebappFoodtap-1.0-SNAPSHOT/WEB-INF/js/script.js
@@ -462,7 +462,10 @@ function topFunction() {
 }
 //Buttom slideToggle
 $(document).ready(function() {
-
+    //tooltip
+    $('[data-toggle="tooltip"]').tooltip();   
+    
+    
     $(".btn-up").click(function() {
         $('.box').slideToggle();
     });
