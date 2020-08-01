965ab6c493db63063f3a372112c81664cd036768
Ben Alpha <benalpha1105@gmail.com>
2020-08-02 04:16:31 +0700

    cm

--- src/main/java/controllers/CouponController.java
+++ src/main/java/controllers/CouponController.java
@@ -6,6 +6,9 @@
 
 package controllers;
 
+import Nghia.Util.RESTHelper;
+import java.io.IOException;
+import models.Coupon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +16,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CouponController {
+    private RESTHelper restHelper;
+
+    public CouponController() throws InstantiationException, IllegalAccessException {
+        restHelper = new RESTHelper(Coupon.class);
+    }
+    
+    
     @RequestMapping(value = "/coupon", method = RequestMethod.GET)
-    public ModelAndView index(){
-        return new ModelAndView("");
+    public ModelAndView index() throws IOException{
+        return new ModelAndView("discount").addObject("coupons",restHelper.getAll());
     }
 }
